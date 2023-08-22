43b30391854f7d7bc3656bfe6cedfaf70a21bcae
hanjo8813 <hanjo8813@gmail.com>
2023-08-22 20:46:48 +0900

    init

---
+++ cicd-test/src/main/java/com/example/cicdtest/CicdTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdTestApplication.class, args);
+    }
+
+}

1dceab66cd91b237263ea70871171a913c47d578
hanjo8813 <hanjo8813@gmail.com>
2023-08-28 22:27:58 +0900

    .

--- src/main/java/com/example/cicdtest/TestController.java
+++ src/main/java/com/example/cicdtest/TestController.java
@@ -1,21 +1,21 @@
-package com.example.cicdtest;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RequestMapping("/test")
-@RestController
-public class TestController {
-
-    @Value("${my-secret}")
-    private String mySecret;
-
-    @GetMapping
-    public ResponseEntity<String> test() {
-        return ResponseEntity.ok(mySecret);
-    }
-
-}
+//package com.example.cicdtest;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RequestMapping("/test")
+//@RestController
+//public class TestController {
+//
+//    @Value("${my-secret}")
+//    private String mySecret;
+//
+//    @GetMapping
+//    public ResponseEntity<String> test() {
+//        return ResponseEntity.ok(mySecret);
+//    }
+//
+//}

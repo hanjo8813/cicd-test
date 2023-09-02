cf76f0d8784117378db41854ea5c0736eaf9b398
hanjo8813 <hanjo8813@gmail.com>
2023-09-02 17:54:44 +0900

    rollback

--- src/test/java/com/example/cicdtest/TempTest.java
+++ src/test/java/com/example/cicdtest/TempTest.java
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.Test;
 
 class TempTest {
 
-    @Test
-    void test() {
+//    @Test
+//    void test() {
 //        TestTarget.test1();
 //        TestTarget.test2();
-    }
-
+//    }
+//
 //    @Test
 //    void testFail() {
 //        assertThat(false).isTrue();
